98c17d108208d9bc16f7fe79f70f1593c4bfa4d4
Kinn-01 <gilmanarief23@gmail.com>
2023-12-12 18:34:36 +0700

    Revert "Membuat Database"
    This reverts commit aad419d4e12879f6764c71331f2e6faba40033e9.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,22 +1,18 @@
 <!doctype html>
-<html lang="en">
+<html>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  {{-- Tailwimd CSS --}}
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
   {{-- Font Google --}}
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@500&display=swap" rel="stylesheet">
-  {{-- !!! --}}
-
-  {{-- title --}}
+  {{--  --}}
   <title>PinnacleBookStore</title>
-
-  {{-- Font Awesome --}}
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
-  
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,800;1,400&display=swap" rel="stylesheet">
@@ -31,10 +27,11 @@
   
         @include('partials.navbar')
 
-            <div class="container">
+            <div class="container mt-4">
                 @yield('container')
             </div>
 
         @include('partials.footer')
+
     </body>
 </html>
