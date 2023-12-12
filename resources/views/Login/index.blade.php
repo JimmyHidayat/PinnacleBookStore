12d252bb1290bee9365d943a36415ec33d51f390
PutriLegianiFahreza <putrilegiani234@gmail.com>
2023-12-12 14:05:30 +0700

    menyelesaikan login

--- resources/views/Login/index.blade.php
+++ resources/views/Login/index.blade.php
@@ -19,7 +19,7 @@
               <h5 class="text-center text-[35px] font-medium text-gray-900 dark:text-gray-600">Login</h5>
               <div>
                   <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600">Your email</label>
-                  <input type="email" name="email" id="email" class="bg-[#D9AE84] border border-yellow-900 text-gray-900 text-sm rounded-lg focus:ring-[#D9AE84] focus:border-[#D9AE84] block w-full p-2.5 dark:bg-[#D9AE84] dark:border-yellow-900 dark:placeholder-yellow-800 dark:text-[#736356]" placeholder="pinnacle@bookstore.com" required>
+                  <input type="email" name="email" id="email" class="bg-[#D9AE84] border border-yellow-900 text-gray-900 text-sm rounded-lg focus:border-[#D9AE84] block w-full p-2.5 dark:bg-[#D9AE84] dark:border-yellow-900 dark:placeholder-yellow-800" placeholder="pinnacle@bookstore.com" required>
               </div>
               <div>
                   <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600">Your password</label>
@@ -32,17 +32,21 @@
                       </div>
                       <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-600">Remember me</label>
                   </div>
-                  <a href="#" class="ms-auto text-sm text-white font-bold hover:underline dark:text-gray-600">Forgot Password?</a>
+                  <a href="#" class="ms-auto text-sm font-bold hover:underline hover:underline hover:text-blue-500 dark:text-black">Forgot Password?</a>
               </div>
-              <button type="submit" class="w-full text-white bg-[#736356] hover:bg-[#736356] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#736356] dark:hover:bg-[#736356] dark:focus:ring-[#736356]">Login to your account</button>
-              <div class="text-sm font-medium text-gray-500 dark:text-gray-600">
-                  Not registered? <a href="#" class="text-[#736356] hover:underline dark:text-white">Create account</a>
+              <button type="submit" class="w-full text-white hover:text-black bg-white hover:bg-[#736356] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#736356] dark:hover:bg-[#EFDECE] dark:focus:ring-[#736356]">Login to your account</button>
+              <div class="text-sm font-medium text-gray-500 dark:text-gray-600 text-capasity-30">
+                  Not registered? <a href="#" class="hover:underline hover:text-blue-500 dark:text-black">Create account</a>
               </div>
-              <div>
-              <a class="px-32 text-center text-sm font-medium text-gray-500 dark:text-gray-600">-Or Continue-</a>
-              <a href="#">
-              <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google logo" class="h-11 w-7 mx-auto my-auto py-2">
-              </a>
+              <div class="flex flex-wrap items-center justify-center">
+                <a class="px-32 text-center text-sm font-medium text-gray-600 mb-2">-Or Continue-</a>
+                <button type="submit" class="flex items-center text-white hover:text-black bg-white hover:bg-[#736356] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#736356] dark:hover:bg-[#EFDECE] dark:focus:ring-[#736356]">
+                    <img
+                        src="img/google.png"
+                        alt=""
+                        class="w-5 h-5 rounded block object-cover align-middle"
+                    > <span class="ml-3 text-md"> Login with google</span>
+                </button>
               </div>
           </form>
       </div>
